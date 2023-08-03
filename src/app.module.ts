fe2319dcd127574e711eb6d495ab95fa672ab7f6
mol <mol@localhost.localdomain>
2023-08-03 08:13:06 -0300

    Add prom metrics

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { MutateModule } from './mutate/mutate.module';
 import { LoggerModule } from './logger/logger.module';
 import { ConfigModule } from './config/config.module';
 import { HealthModule } from './health/health.module';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [ConfigModule, LoggerModule, HealthModule, MutateModule],
+  imports: [ConfigModule, LoggerModule, HealthModule, MutateModule, PrometheusModule.register()],
 })
 export class AppModule { }
